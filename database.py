976904e8ff23d5f9b649d4dd5d5e80c165bcb768
aman-devTech <this.side.aman.raj@gmail.com>
2026-02-11 16:48:49 +0530

    use env database url

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-db_url = "postgresql://postgres:password@localhost:5432/fastAPI_beginner"
+db_url = os.getenv("postgresql://fastapi_db_93m1_user:tFqfEoZCAeFE4JaCaMfJuJ3GlgjOag0g@dpg-d6668mi4d50c73b4he3g-a.oregon-postgres.render.com/fastapi_db_93m1")
 engine = create_engine(db_url)
 # session is cycle of server to database or to client or vice versa
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine) 
